00f65ce4f84f685bbece780b057618ee099ff5fc
admin <admin@m18031643.rz.krzn.de>
2013-05-26 11:26:42 +0200

    new

--- http/myHttpServer.py
+++ http/myHttpServer.py
@@ -9,12 +9,36 @@ from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
 
 
 class BaseRequestHandler(BaseHTTPRequestHandler):
-    pass
+    
+    def sendFileToBrowser(self, fileName, contentType="text/html"):
+        f = open(fileName)
+        self.send_response(200)
+        self.send_header("Content-Type", contentType)
+        self.end_headers()
+        self.wfile.write(f.read())
+        f.close
+        
+    
+    def do_signIn(self):
+        fileName = curdir + sep + "files" + sep + "html" + sep + "loginform.html"
+        self.sendFileToBrowser(fileName)
+    
+    def do_GET(self):
+        try:
+            ctype, pdict = cgi.parse_header(self.headers.getheader('cookie',""))
+            if ctype == "":
+                self.do_signIn()
+
+        except IOError:
+            self.send_error(404, "not found!")
 
 def main():
     print("starting http-server on port 80...")
     try:
         server = HTTPServer(('', 80), BaseRequestHandler)
+        print("started server!")
+        server.serve_forever()
+        
     except KeyboardInterrupt:
         print("^C shutting down server...")
         server.socket.close()
