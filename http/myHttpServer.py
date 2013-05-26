2c6f646ad3c03f3ff3c9aa0af33709ceb190db54
admin <admin@m18031643.rz.krzn.de>
2013-05-26 10:58:42 +0200

    .

--- http/myHttpServer.py
+++ http/myHttpServer.py
@@ -3,7 +3,21 @@ Created on 26.05.2013
 
 @author: admin
 '''
+import string,cgi,time
+from os import curdir,sep
+from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
+
+
+class BaseRequestHandler(BaseHTTPRequestHandler):
+    pass
+
+def main():
+    print("starting http-server on port 80...")
+    try:
+        server = HTTPServer(('', 80), BaseRequestHandler)
+    except KeyboardInterrupt:
+        print("^C shutting down server...")
+        server.socket.close()
 
 if __name__ == '__main__':
-    print("hello world!")
-    pass
+    main()
